0e743651089611b219caeba47c6e17c4135e7bec
Adil Poyraz AKA <workintech.poyrazaka@gmail.com>
2026-06-08 18:44:24 +0300

    feat(utils): euclidean distance

--- utils/distances.py
+++ utils/distances.py
@@ -2,3 +2,8 @@ def manhattan(a, b):
     d_x = b[0] - a[0]
     d_y = b[1] - a[1]
     return abs(d_x) + abs(d_y)
+
+def euclidean(a, b):
+    d_x = b[0] - a[0]
+    d_y = b[1] - a[1]
+    return (d_x ** 2 + d_y ** 2) ** 0.5
