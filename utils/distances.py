29ac072c57e934ffeec744ed7152ee34d5138c41
Adil Poyraz AKA <workintech.poyrazaka@gmail.com>
2026-06-08 18:44:55 +0300

    refactor(utils): manhattan and euclidean via minkowski

--- utils/distances.py
+++ utils/distances.py
@@ -1,9 +1,12 @@
 def manhattan(a, b):
-    d_x = b[0] - a[0]
-    d_y = b[1] - a[1]
-    return abs(d_x) + abs(d_y)
+    return minkowski(a, b, 1)
+
 
 def euclidean(a, b):
+    return minkowski(a, b, 2)
+
+
+def minkowski(a, b, p):
     d_x = b[0] - a[0]
     d_y = b[1] - a[1]
-    return (d_x ** 2 + d_y ** 2) ** 0.5
+    return (abs(d_x) ** p + abs(d_y) ** p) ** (1 / p)
